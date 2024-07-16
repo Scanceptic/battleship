cd97f636801d83266f6c9c558ed8d9239d8c64ae
Nathan <urwin.nathan@gmail.com>
2024-07-16 09:25:03 +0100

    create placeShip func and test

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const { create } = require("domain");
+
+// create a battleship with a given length
 function createShip(length) {
-	const ship = {
+	const Ship = {
 		length,
 		hits: 0,
 		sunk: false,
@@ -12,11 +15,58 @@ function createShip(length) {
 			return this.sunk;
 		},
 	};
-	return ship;
+	return Ship;
+}
+
+// create the gameboard
+function createGameboard() {
+	function createGrid() {
+		const grid = [];
+		const row = new Array(10);
+		for (let index = 0; index < 10; index++) {
+			grid.push(row);
+		}
+		return grid;
+	}
+	const Gameboard = {
+		grid: createGrid(),
+		placeShip: function (x, y, length) {
+			const Ship = createShip(length);
+			if (y + Ship.length - 1 < 10) {
+				for (let index = 0; index < Ship.length; index++) {
+					this.grid[y + index][x] = Ship;
+				}
+				return this.grid[y + Ship.length][x];
+			} else if (x + Ship.length - 1 < 10) {
+				for (let index = 0; index < Ship.length; index++) {
+					this.grid[y][x + index] = Ship;
+				}
+				return this.grid[y][x + Ship.length];
+			} else if (y - Ship.length + 1 >= 0) {
+				for (let index = 0; index < Ship.length; index++) {
+					this.grid[y - index][x] = Ship;
+				}
+				return this.grid[y - Ship.length][x];
+			} else if (x - Ship.length + 1 >= 0) {
+				for (let index = 0; index < Ship.length; index++) {
+					this.grid[y][x - index] = Ship;
+				}
+				return this.grid[y][x - Ship.length];
+			}
+			return false;
+		},
+		receiveAttack: function (x, y) {},
+		missedAttacks: [],
+		allShipsSunk: false,
+	};
+
+	return Gameboard;
 }
 
 const hitTestShip = createShip(3);
 
 const sunkTestShip = createShip(0);
 
-module.exports = { hitTestShip, sunkTestShip };
+const Gameboard = createGameboard();
+
+module.exports = { hitTestShip, sunkTestShip, Gameboard };
--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const { hitTestShip, sunkTestShip } = require("./app.js");
+const { hitTestShip, sunkTestShip, Gameboard } = require("./app.js");
 
 test("When ship is hit, increments ship.hits prop by 1 from 0 to 1", () => {
 	expect(hitTestShip.hit()).toBe(1);
@@ -6,4 +6,20 @@ test("When ship is hit, increments ship.hits prop by 1 from 0 to 1", () => {
 
 test("When ship length is 0, hits equal length and ship.sunk === true", () => {
 	expect(sunkTestShip.isSunk()).toBe(true);
+});
+
+test("When gameboard is created, the grid is an array of length 10", () => {
+	expect(Gameboard.grid.length).toBe(10);
+});
+
+test("When gameboard is created, the grid is an array with subarrays of length 10", () => {
+	expect(Gameboard.grid[0].length).toBe(10);
+});
+
+test("When gameboard places ship at position [1,3], a new ship object is created at array[3][1]", () => {
+	expect(typeof Gameboard.placeShip(1, 3, 1)).toBe("object");
+});
+
+test("When gameboard places ship at position [2,4] of length 3 the ship position extends to array[6][2]", () => {
+	expect(typeof Gameboard.placeShip(2, 4, 3)).toBe("object");
 });
